11ee31ab9a2d480f1671be636a6557346156b2e4
nandhini <nandhini1997ece@gmail.com>
2024-09-22 13:27:12 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from './components/Header'
+import LandingSection from './components/LandingSection'
+import FeaturesSection from './components/FeaturesSection'
+
+const App = () => (
+  <>
+    <Header />
+    <LandingSection />
+    <FeaturesSection />
+  </>
+)
+export default App
